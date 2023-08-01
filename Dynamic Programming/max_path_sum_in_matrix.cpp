d5db4de3ad470f36b64baa12b724c54a43cb1a0e
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2023-08-01 16:21:44 +0530

    CodingNinjas

--- Dynamic Programming/max_path_sum_in_matrix.cpp
+++ Dynamic Programming/max_path_sum_in_matrix.cpp
@@ -6,7 +6,6 @@ int getMaxPathSum(vector<vector<int>> &matrix)
     int n = matrix.size();
     int m = matrix[0].size();
     vector<vector<int>> dp(n,vector<int>(m,0));
-    int left_diag,right_diag;
     for(int j=0;j<m;j++)
     {
         dp[n-1][j] = matrix[n-1][j];
@@ -16,14 +15,8 @@ int getMaxPathSum(vector<vector<int>> &matrix)
         for(int j=m-1;j>=0;j--)
         {
             int down = matrix[i][j] + dp[i+1][j];
-            if(j>0)
-                left_diag = matrix[i][j] + dp[i+1][j-1];
-            else
-                left_diag = INT_MIN;
-            if(j<m-1)
-                right_diag = matrix[i][j] + dp[i+1][j+1];
-            else
-                right_diag = INT_MIN;
+            int left_diag = (j>0) ? (matrix[i][j] + dp[i+1][j-1]):INT_MIN; //conditional way uses less memory
+            int right_diag = (j<m-1) ? (matrix[i][j] + dp[i+1][j+1]):INT_MIN;
             dp[i][j] = max({down,left_diag,right_diag});
             
         }

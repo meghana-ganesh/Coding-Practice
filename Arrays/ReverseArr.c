b0dc789cb21bcd00d03b84f914e2e9d08f49f74c
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2023-08-17 22:32:55 +0530

    CodingNInjas

--- Arrays/ReverseArr.c
+++ Arrays/ReverseArr.c
@@ -1,3 +1,24 @@
+//optimized:Swap func uses two-pointer approach(For built-in types, it uses efficient assembly instructions or memory copying techniques. 
+//For user-defined types, 
+//it leverages specialized template functions or the swap overload to provide efficient swapping mechanisms.)
+#include <bits/stdc++.h> 
+void reverseArray(vector<int> &arr , int m)
+{
+	// Write your code here.
+	int n = arr.size();
+	int left = m + 1;
+	int right = n - 1;
+	while(left < right)
+	{
+		swap(arr[left],arr[right]);
+		left++;
+		right--;
+	}
+}
+//1
+//1 2 3 4 5 6
+
+//my code:
 #include <bits/stdc++.h> 
 void reverseArray(vector<int> &arr , int m)
 {

e5460298238063760ba8b3769c42a3c317397fe5
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2023-09-10 15:22:08 +0530

    CodingNInjas

--- Sliding Window And Two Pointer/LongestSubstrWithoutRepeatingCharacters.c
+++ Sliding Window And Two Pointer/LongestSubstrWithoutRepeatingCharacters.c
@@ -1,1 +1,28 @@
-
+//O(N^2) APPROACH:
+#include <bits/stdc++.h> 
+int uniqueSubstrings(string str)
+{
+    //Write your code here
+    int n = str.size();
+    if(n == 0)
+        return 0;
+    int max_ans =  INT_MIN;
+    int isrepeating = 0;
+    for(int i=0;i<n;i++)
+    {
+        unordered_set<char> set;
+        for(int j=i;j<n;j++)
+        {
+            if(set.find(str[j]) != set.end())
+            {
+                isrepeating = 1;
+                max_ans = max(max_ans,j-i);
+                break;
+            }
+            set.insert(str[j]);
+        }
+    }
+    if(isrepeating == 0)
+        return n;
+    return max_ans;
+}

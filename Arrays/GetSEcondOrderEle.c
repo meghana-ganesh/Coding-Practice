f24cfff0edd6aff5ba6d65099cde3a9e081a2e0f
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2023-10-03 17:43:12 +0530

    CodingNinjas

---
+++ Arrays/GetSEcondOrderEle.c
@@ -0,0 +1,29 @@
+vector<int> getSecondOrderElements(int n, vector<int> a) 
+{
+    // Write your code here.
+    int maxele = INT_MIN;
+    int secmax = INT_MIN;
+    int minele = INT_MAX;
+    int secmin = INT_MAX;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i] >= maxele)
+        {
+            secmax = maxele;
+            maxele = a[i];
+        }
+        else if(a[i] > secmax && a[i] != maxele)
+            secmax = a[i];
+        if(a[i] <= minele)
+        {
+            secmin = minele;
+            minele = a[i];
+        }
+        else if(a[i] < secmin && a[i] != minele)
+            secmin = a[i];
+    }
+    vector<int> res;
+    res.push_back(secmax);
+    res.push_back(secmin);
+    return res;
+}

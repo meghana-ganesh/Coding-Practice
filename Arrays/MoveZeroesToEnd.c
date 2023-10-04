15e551d24c970173cb8a2ffb4ff1fc9e23518430
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2023-10-04 09:39:44 +0530

    CodingNinjas

---
+++ Arrays/MoveZeroesToEnd.c
@@ -0,0 +1,16 @@
+//TIME COMPLEXITY:O(N)
+//SPACE COMPLEXITY:O(1)
+vector<int> moveZeros(int n, vector<int> a) 
+{
+    // Write your code here.
+    int j=0;
+    for(int i=0;i<n;i++)
+    {
+        if(a[i] != 0)
+        {
+            swap(a[i],a[j]);
+            j++;
+        }
+    }   
+    return a;
+}

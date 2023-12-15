56a6a4ff62d05b4d115c7416142caaf24b4a99b6
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2023-12-14 20:24:55 -0800

    CodingNinjas-GQ

--- BinarySearch/1DArrays/SingleElementInASortedArray.cpp
+++ BinarySearch/1DArrays/SingleElementInASortedArray.cpp
@@ -1,3 +1,43 @@
+//iterative binary search
+int binary_search(vector<int> &arr,int n,int l,int u)
+{
+	while(l<=u)
+	{
+		int mid = l + (u-l)/2;
+		if(arr[mid-1] != arr[mid] && arr[mid+1] != arr[mid])
+		{
+			return arr[mid];
+		}
+		if((mid%2==1) && arr[mid-1] == arr[mid])
+		{
+			l = mid+1;
+		}
+		else if((mid%2==0) && arr[mid+1] == arr[mid])
+		{
+			l = mid+1;
+		}
+		else
+			u = mid-1;
+	}
+}
+
+
+int singleNonDuplicate(vector<int>& arr)
+{
+	// Write your code here
+	int n = arr.size();
+	if(arr[0] != arr[1])
+		return arr[0];
+	else if(arr[n-1] != arr[n-2])
+		return arr[n-1];
+	return binary_search(arr,n,0,n-1);
+}
+
+
+
+
+
+//recursive binary search
 int binary_search(vector<int> &arr,int n,int l,int u)
 {
 	if(l<=u)

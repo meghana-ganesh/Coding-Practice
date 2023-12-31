1825e4d9037be8c654c6caa48a842a0c37d6cd43
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2023-12-31 13:52:58 +0530

    CodingNinjas-Microsoft's Question(Easy-tricky logic)

---
+++ BinarySearch/2DArrays/RowWithMax1s.cpp
@@ -0,0 +1,33 @@
+int lowerBound(vector<int> arr,int n,int x)
+{
+    int l=0,u=n;
+    while(l <= u)
+    {
+        int mid = l + (u-l)/2;
+        if(arr[mid] >= x)
+            u = mid-1;
+        else if(arr[mid] < x)
+            l = mid+1;       
+    }
+    return l;
+}
+//we can take lowerbound(1) or upperbound(0)
+//lowerbound(x) is index such that arr[ind] >= x
+//upperbound(x) is index such that arr[ind] > x
+
+int rowWithMax1s(vector<vector<int>> &matrix, int n, int m)
+{
+    //    Write your code here.
+    int cnt_max = -1;
+    int index = -1;
+    for(int i=0;i<n;i++)
+    {
+        int cnt_ones = m - lowerBound(matrix[i],m,1);
+        if(cnt_ones > cnt_max)
+        {
+            index = i;
+            cnt_max = cnt_ones;
+        }
+    }
+    return index;
+}

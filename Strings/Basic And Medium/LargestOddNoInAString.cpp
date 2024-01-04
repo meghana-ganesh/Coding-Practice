7eb648d82af059ccf5448a3c3b5381746aa20603
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2024-01-04 19:04:52 +0530

    Leetcode

---
+++ Strings/Basic And Medium/LargestOddNoInAString.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string largestOddNumber(string num) 
+    {
+        string ans = "";
+        int n = num.length();
+        for(int i=n;i>=0;i--)
+        {
+            if(num[i] % 2 == 1)
+            {
+                return num.substr(0,i+1);
+            }
+        }
+        return "";
+    }
+};

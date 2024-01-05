5a08cfd61b9795f6a36864a5ac4fd03a8c752c22
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2024-01-05 09:44:58 +0530

    LeetCode+CodingNinjas-VGQ(Amazon & Microsoft's Ques)

--- Strings/Basic And Medium/CheckIfOneStringIsARotationOfAnotherString.cpp
+++ Strings/Basic And Medium/CheckIfOneStringIsARotationOfAnotherString.cpp
@@ -1,4 +1,38 @@
+//Best Approach:Two Pointer,Passed on CodingNinjas with 77% efficiency
+//TIME COMPLEXITY:O(L1)
+//SPACE COMPLEXITY:O(L1) //STRING S = P + P
+int isCyclicRotation(string &p, string &q) 
+{
+    // Write your code here
+    string s = p + p;
+    int i=0,j=0;
+    int l1 = s.size();
+    int l2 = q.size();
+    if(p.size() != l2)
+        return 0;
+    while(i<=l1)
+    {
+        if(s[i] == q[j])
+        {
+            j++;
+            i++;
+            if(j==l2)
+                return 1;
+            continue;
+        }
+        if(s[i] != q[j] && j!=0)
+        {
+            j = 0;
+            continue;
+        }
+        i++;
+    }
+    return 0;
+}
+
+
 //Passed on leetcode,better than 35%,49/50 testcases on CodingNinjas
+//TIME COMPLEXITY:O(L1^2)
 class Solution {
 public:
     bool rotateString(string p, string q) 
@@ -13,6 +47,8 @@ public:
 };
 
 //Passed on Leetcode,better than 7%,44/50 testcases on CodingNinjas
+//TIME COMPLEXITY:O(L1^2) //L1 FOR SUBSTR FUNC & L1 FOR FOR LOOP
+//SPACE COMPLEXITY:O(L1)
 int isCyclicRotation(string &p, string &q) 
 {
     // Write your code here

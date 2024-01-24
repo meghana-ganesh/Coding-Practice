93b9b2351341cd9f4b12bf7fb02ae4cecc807dad
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2024-01-24 09:11:32 +0530

    CodingNinjas-Iterative+Recursive

--- LinkedList/ReverseLL.cc
+++ LinkedList/ReverseLL.cc
@@ -1,37 +1,24 @@
-/****************************************************************
+//RECURSIVE
+//TIME COMPLEXITY-O(N)
+//SPACE COMPLEXITY-O(N)
+Node* reverseLinkedList(Node *head)
+{
+    // Write your code here
+    if(head == nullptr || head->next == nullptr)
+        return head;
+    Node *newhead = reverseLinkedList(head->next);
+    Node *cur = head->next;
+    cur->next = head;
+    head->next = nullptr;
+    return newhead;
+}
 
-    Following is the class structure of the Node class:
 
-        class Node
-        {
-        public:
-            int data;
-            Node *next;
-            Node()
-            {
-                this->data = 0;
-                next = NULL;
-            }
-            Node(int data)
-            {
-                this->data = data;
-                this->next = NULL;
-            }
-            Node(int data, Node* next)
-            {
-                this->data = data;
-                this->next = next;
-            }
-        };
-        
-
-*****************************************************************/
-//1 2 3 4
+//ITERATIVE
+//TIME COMPLEXITY:O(N),SPACE COMPLEXITY:O(N)
 Node* reverseLinkedList(Node *root)
 {
     // Write your code here
-    if(root == nullptr || root->next == nullptr)
-        return root;
     Node *cur = root;
     Node *prev = nullptr;
     Node *next = nullptr;
@@ -44,4 +31,27 @@ Node* reverseLinkedList(Node *root)
     }
     root = prev;
     return root;
+}
+
+//TIME COMPLEXITY:O(N)+O(N) = O(2N)
+//SPACE COMPLEXITY:O(N) - use of stack
+#include<stack>
+Node* reverseLinkedList(Node *head)
+{
+    // Write your code here
+    stack<int> st;
+    Node *rear = head;
+    while(rear)
+    {
+        st.push(rear->data);
+        rear = rear->next;
+    }
+    rear = head;
+    while(rear && !st.empty())
+    {
+        rear->data = st.top();
+        st.pop();
+        rear = rear->next;
+    }
+    return head;
 }

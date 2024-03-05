b07dc40d67d6b598349c70f8f5893dd8d8224e33
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2024-03-05 08:31:02 +0530

    GQ-Check if LL is palindrome

--- LinkedList/CheckIfLLisPalindromeOrNot.cc
+++ LinkedList/CheckIfLLisPalindromeOrNot.cc
@@ -26,4 +26,49 @@ bool isPalindrome(Node *head)
 }
 
 //Optimized approach
-
+//O(N) without additional space
+Node* reverseLL(Node *head)
+{
+    if(head == nullptr || head->next == nullptr)
+        return head;
+    Node *cur = head;
+    Node *prev = nullptr;
+    Node *next = nullptr;
+    while(cur)
+    {
+        next = cur->next;
+        cur->next = prev;
+        prev = cur;
+        cur = next;
+    }
+    head = prev;
+    return head;
+}
+//1 2 3 4
+bool isPalindrome(Node *head)
+{
+    // write your code here
+    Node *slow = head;
+    Node *fast = head;
+    Node *cur = head;
+    while(fast->next != nullptr && fast->next->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    Node *revhead = reverseLL(slow->next);
+    Node *first = head;
+    Node *second = revhead;
+    while(second)
+    {
+        if(first->data != second->data)
+        {
+            reverseLL(revhead);
+            return false;
+        }
+        first = first->next;
+        second = second->next;
+    }
+    reverseLL(revhead);
+    return true;
+}

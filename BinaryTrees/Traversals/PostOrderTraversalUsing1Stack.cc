5d3364519a6cc064a769271dacaa4a7f55ab9eba
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2024-05-28 07:01:28 +0530

    LeetCode - (Learn/Revise again) - Tough

---
+++ BinaryTrees/Traversals/PostOrderTraversalUsing1Stack.cc
@@ -0,0 +1,51 @@
+//TC:O(2N)
+//SC:O(N)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) 
+    {
+        vector<int> ans;
+        stack<TreeNode*> st;
+        TreeNode *cur = root;
+        TreeNode *temp;
+        while(cur != NULL || !st.empty())
+        {
+            if(cur != NULL)
+            {
+                st.push(cur);
+                cur = cur->left;
+            }
+            else
+            {
+                temp = st.top()->right;
+                if(temp == NULL)
+                {
+                    temp = st.top();
+                    st.pop();
+                    ans.push_back(temp->val);
+                    while(!st.empty() && temp == st.top()->right)
+                    {
+                        temp = st.top();
+                        st.pop();
+                        ans.push_back(temp->val);
+                    }
+                }
+                else
+                    cur = temp;
+            }
+        }
+    return ans;
+    }
+};

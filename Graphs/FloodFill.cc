564fb553b9a1751fba4695ef26dc6dda7021d191
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2024-07-23 08:34:55 +0530

    LeetCode-VGQ

--- Graphs/FloodFill.cc
+++ Graphs/FloodFill.cc
@@ -26,3 +26,41 @@ public:
         return ans;
     }
 };
+
+
+//USING BFS
+class Solution {
+public:
+    void bfs(vector<vector<int>>& image, int sr, int sc, int newColor,int n,int m)
+    {
+        int iniColor = image[sr][sc];
+        queue<pair<int,int>> q;
+        q.push({sr,sc});
+        image[sr][sc] = newColor;
+        vector<int> deltaRow = {-1,0,+1,0};
+        vector<int> deltaCol = {0,-1,0,+1};
+        while(!q.empty())
+        {
+            auto [row,col] = q.front();
+            q.pop();
+            for(int i=0;i<4;i++)
+            {
+                int nrow = row + deltaRow[i];
+                int ncol = col + deltaCol[i];
+                if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m && image[nrow][ncol] == iniColor && image[nrow][ncol] != newColor)
+                {
+                    image[nrow][ncol] = newColor;
+                    q.push({nrow,ncol});
+                }
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if (image[sr][sc] == color) 
+            return image;
+        int n = image.size();
+        int m = image[0].size();
+        bfs(image,sr,sc,color,n,m);
+        return image;
+    }
+};

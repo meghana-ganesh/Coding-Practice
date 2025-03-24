e9dd259f50293b204ae060cd1bd72079ae168328
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2025-03-24 11:13:23 +0530

    Update selectionsort.c

--- Sorting Techniques/selectionsort.c
+++ Sorting Techniques/selectionsort.c
@@ -1,14 +1,13 @@
-void selectionSort(vector<int>&arr) 
-{
-    // Write your code here.
-    for(int i=0;i<arr.size();i++)
-    {
-        for(int j=i+1;j<arr.size();j++)
-        {
-            if(arr[i] > arr[j])
-            {
-                swap(arr[i],arr[j]);
+void selectionSort(vector<int> &arr) {
+        // code here
+        int i,j;
+        for(i=0;i<arr.size();i++){
+            int min = i;
+            for(j=i+1;j<=arr.size()-1;j++){
+                if(arr[j]<arr[min]){
+                    min = j;
+                }
             }
+            swap(arr[i],arr[min]);
         }
     }
-}

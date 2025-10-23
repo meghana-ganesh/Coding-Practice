969f21d5e1d9fb702e62e71d696ff69061da74ac
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2025-10-23 20:42:03 +0530

    Leetcode - Easy

---
+++ Leetcode POTD/CheckIfDigitsAreEqualinStringAfterOperationsI.cc
@@ -0,0 +1,19 @@
+//TC:O(N^2)
+class Solution {
+public:
+    string modOperation(string s)
+    {
+        if(s.length() == 2)
+            return s;
+        for(int i=0;i<s.length()-1;i++)
+        {
+            s[i] = (((s[i] - '0') + (s[i+1] - '0')) % 10) + '0';
+        }
+        s.pop_back();
+        return modOperation(s);
+    }
+    bool hasSameDigits(string s) {
+        s = modOperation(s);
+        return s[0] == s[1];
+    }
+};

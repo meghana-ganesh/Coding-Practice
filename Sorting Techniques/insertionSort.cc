64f4c1bc5715a3a03fcdcf40c4658c6d19d43cbd
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2026-07-12 13:15:54 +0530

    Striver - Leetcode

---
+++ Sorting Techniques/insertionSort.cc
@@ -0,0 +1,19 @@
+//insertion sort, inserts element at correct order, keeps checking toward left(j--)
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& arr) 
+    {
+        int n = arr.size();
+        int i,j;
+        for(int i=0;i<n;i++)
+        {
+            j = i;
+            while(j > 0 && arr[j-1] > arr[j])
+            {
+                swap(arr[j],arr[j-1]);
+                j--;
+            }
+        }
+        return arr;
+    }
+};

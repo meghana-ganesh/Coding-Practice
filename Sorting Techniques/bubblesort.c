011acd51442d306a94ceda2d04ca6714d8146f1a
Meghana Ganesh <46048320+meghana-ganesh@users.noreply.github.com>
2026-07-12 13:00:35 +0530

    Update bubblesort.c

--- Sorting Techniques/bubblesort.c
+++ Sorting Techniques/bubblesort.c
@@ -3,12 +3,20 @@
 void bubbleSort(int arr[], int n) 
 {
     //write your code here
+    int didSwap = 0; //for optimised bubble sort
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n-i-1;j++)
         {
             if(arr[j] > arr[j+1])
+            {
                 swap(arr[j],arr[j+1]);
+                int didSwap = 1;
+            }
         }
+        //optimised bubble sort
+        if(didSwap == 0)
+            break;
+            
     }
 }
